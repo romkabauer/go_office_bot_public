0cbdce39add41673ce3bbd788d592131eba3248c
romkabauer <pm.rvbauer@gmail.com>
2021-07-05 18:48:27 +0300

    tested fix

--- pool_settings.py
+++ pool_settings.py
@@ -3,5 +3,5 @@ from datetime import timedelta
 
 tomorrow = datetime.date.today() + timedelta(days=1)
 
-pool_question = f'{tomorrow.strftime("%A, %d + 1 %B")} 🏢🚶‍♂️?'
+pool_question = f'{tomorrow.strftime("%A, %d %B")} 🏢🚶‍♂️?'
 pool_options = ["Zastavskaya, 22, K2A", "Pevcheskiy, 12", "B.Posadskaya, 12", "Nab. Chyornoy rechki, 41", "Show results"]
