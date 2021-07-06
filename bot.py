dffcd77e836a0ff8250c1dfa1c64aa6c67a96255
romkabauer <pm.rvbauer@gmail.com>
2021-07-06 18:49:34 +0300

    fix day+1

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ dp.middleware.setup(LoggingMiddleware())
 
 session: aiohttp.ClientSession = aiohttp.ClientSession()
 
-chat_id_storage_path = 'chats_to_handle_test.txt'
+chat_id_storage_path = 'chats_to_handle.txt'
 
 async def update_s3_storage_file(content):
     client = boto3.client(
@@ -85,7 +85,7 @@ async def create_pool():
 async def scheduler():
     # aioschedule.every().minute.do(create_pool)
     aioschedule.every().day \
-                       .at("15:47") \
+                       .at("15:00") \
                        .do(create_pool)
     while True:
         await aioschedule.run_pending()
