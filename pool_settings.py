d8f97544f76dc1af1963f78e586aa20846f7ff35
romkabauer <pm.rvbauer@gmail.com>
2021-07-07 18:32:01 +0300

    fix misspelling in options

--- pool_settings.py
+++ pool_settings.py
@@ -1,1 +1,1 @@
-pool_options = ["Zastavskaya, 22, K2A", "Pevcheskiy, 12", "B.Posadskaya, 12", "Nab. Chyornoy rechki, 41", "Show results"]
+pool_options = ["Zastavskaya, 22, K2A", "Pevcheskiy, 12", "B. Posadskaya, 12", "Nab. Chyornoy rechki, 41", "Show results"]
