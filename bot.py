02feabfbac156985ef157469783008ee039ae0db
romkabauer <pm.rvbauer@gmail.com>
2021-07-18 21:27:02 +0300

    revert

--- bot.py
+++ bot.py
@@ -98,7 +98,7 @@ async def create_pool():
 async def scheduler():
     # aioschedule.every().minute.do(create_pool)
     aioschedule.every().day \
-                       .at("18:26") \
+                       .at("15:00") \
                        .do(create_pool)
     while True:
         await aioschedule.run_pending()
